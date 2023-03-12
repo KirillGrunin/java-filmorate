faa6d60f8e0ea7ce91e7aa19f3e27a931c07f8d4
KirillGrunin <krgrunin@gmail.com>
2023-03-12 15:49:38 +0300

    Спринт 10, отправка на 1 ревью

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -10,8 +11,11 @@ import lombok.Data;
 import ru.yandex.practicum.filmorate.annotations.CorrectLogin;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
+@Valid
 public class User {
     @PositiveOrZero(message = "id can not be negative")
     private int id;
@@ -31,4 +35,20 @@ public class User {
      */
     @PastOrPresent
     private LocalDate birthday;
+
+    private Set<Integer> friends;
+
+    public void addFriend(Integer id) {
+        if (friends == null) {
+            friends = new HashSet<>();
+        }
+        friends.add(id);
+    }
+
+    public Set<Integer> getFriendsId() {
+        if (friends == null) {
+            friends = new HashSet<>();
+        }
+        return friends;
+    }
 }
